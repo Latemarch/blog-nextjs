5ce5b86deb842967f793395956299e97cedb40e8
Latemarch <73418661+Latemarch@users.noreply.github.com>
2023-08-13 16:28:33 +0900

    kill fetching local data

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -2,7 +2,6 @@ import MarkdowkVeiwer from '@/components/MarkdowkVeiwer'
 import { formatDate } from '@/components/PostCard'
 import Tag from '@/components/Tag'
 import { getItem, getItems } from '@/service/fireabse'
-import { getPost } from '@/service/posts'
 import { notFound } from 'next/navigation'
 
 interface Props {
@@ -25,7 +24,7 @@ export async function generateMetadata({ params: { id } }: Props) {
 }
 
 export default async function page({ params: { id } }: Props) {
-  const post = await getPost(id)
+  const post = await getItem(id, 'posts')
   if (!post) return notFound()
   return (
     <article className="m-0 pb-20 text-zinc-800 dark:text-Dspan w-full">
@@ -38,7 +37,7 @@ export default async function page({ params: { id } }: Props) {
         </div>
         <ul className="flex mt-2 space-x-2">
           {post.tags &&
-            post.tags.map((tag) => (
+            post.tags.map((tag: string) => (
               <Tag key={tag} name={tag} style={{ color: '#37D4BF' }} />
             ))}
         </ul>
--- service/posts.ts
+++ service/posts.ts
@@ -1,32 +1,32 @@
-import { IPost } from "@/type";
-import fs from "fs";
-import path from "path";
-import { cache } from "react";
+// import { IPost } from "@/type";
+// import fs from "fs";
+// import path from "path";
+// import { cache } from "react";
 
-export const getAllPosts = cache(async (listLen?: number): Promise<IPost[]> => {
-	// export async function getAllPosts(listLen?: number): Promise<IPost[]> {
-	const filePath = path.join(process.cwd(), "public", "datas", "posts.json");
-	const fileContents = fs.readFileSync(filePath, "utf8");
-	const posts: IPost[] = JSON.parse(fileContents);
+// export const getAllPosts = cache(async (listLen?: number): Promise<IPost[]> => {
+// 	// export async function getAllPosts(listLen?: number): Promise<IPost[]> {
+// 	const filePath = path.join(process.cwd(), "public", "datas", "posts.json");
+// 	const fileContents = fs.readFileSync(filePath, "utf8");
+// 	const posts: IPost[] = JSON.parse(fileContents);
 
-	const sortedPosts = listLen
-		? Object.values(posts)
-				.map((el) => el)
-				.sort((a, b) => b.createdAt - a.createdAt)
-				.slice(0, listLen)
-		: Object.values(posts)
-				.map((el) => el)
-				.sort((a, b) => b.createdAt - a.createdAt);
+// 	const sortedPosts = listLen
+// 		? Object.values(posts)
+// 				.map((el) => el)
+// 				.sort((a, b) => b.createdAt - a.createdAt)
+// 				.slice(0, listLen)
+// 		: Object.values(posts)
+// 				.map((el) => el)
+// 				.sort((a, b) => b.createdAt - a.createdAt);
 
-	return sortedPosts;
-});
+// 	return sortedPosts;
+// });
 
-export const getPost = cache(async (id: string): Promise<IPost> => {
-	// export async function getPost(id: string): Promise<IPost> {
-	const filePath = path.join(process.cwd(), "public", "datas", "posts.json");
-	const fileContents = fs.readFileSync(filePath, "utf8");
-	const posts = JSON.parse(fileContents);
-	const post = posts[id];
-	// if (!post) throw new Error(`${id}에 해당하는 post없음`);
-	return post;
-});
+// export const getPost = cache(async (id: string): Promise<IPost> => {
+// 	// export async function getPost(id: string): Promise<IPost> {
+// 	const filePath = path.join(process.cwd(), "public", "datas", "posts.json");
+// 	const fileContents = fs.readFileSync(filePath, "utf8");
+// 	const posts = JSON.parse(fileContents);
+// 	const post = posts[id];
+// 	// if (!post) throw new Error(`${id}에 해당하는 post없음`);
+// 	return post;
+// });
