7dcc1ec9452ca8b21ce47310a2c965d8d07b66d0
Latemarch <73418661+Latemarch@users.noreply.github.com>
2023-08-20 21:25:09 +0900

    feat: change default theme of darkmodeswitch

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,28 +1,29 @@
-"use client";
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
-import { DarkModeSwitch } from "react-toggle-dark-mode";
+'use client'
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
+import { DarkModeSwitch } from 'react-toggle-dark-mode'
 
 export default function ThemeSwitch() {
-	const { theme, setTheme } = useTheme();
-	const [mounted, setMounted] = useState(false);
-	const handleClick = () => {
-		setTheme(theme === "dark" ? "light" : "dark");
-	};
-	useEffect(() => {
-		setMounted(true);
-	}, []);
-	if (!mounted) {
-		return null;
-	}
+  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+  const handleClick = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
+  useEffect(() => {
+    setMounted(true)
+    if (!theme) setTheme('dark')
+  }, [])
+  if (!mounted) {
+    return null
+  }
 
-	return (
-		<DarkModeSwitch
-			className="mr-4 "
-			onChange={handleClick}
-			checked={theme === "dark" ? true : false}
-			sunColor={"gray"}
-			moonColor={"white"}
-		/>
-	);
+  return (
+    <DarkModeSwitch
+      className="mr-4 "
+      onChange={handleClick}
+      checked={theme === 'dark' ? true : false}
+      sunColor={'gray'}
+      moonColor={'white'}
+    />
+  )
 }
