9f15386a0cf4e3383c4fbc5cc4dda7b02270b0f5
Latemarch <dolphin0331@naver.com>
2023-09-05 13:22:39 +0900

    minor refactor

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,34 +2,34 @@
 import { INav } from "@/type";
 import Link from "next/link";
 import Button from "./Button";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export default function Nav({ buttons }: INav) {
-	const pathname = usePathname();
-	const location = pathname.split("/")[1];
-	const category = location === "" ? "home" : location;
-	return (
-		<ul className="relative z-50 flex px-2 text-h1 dark:text-white drop-shadow-md rounded-full bg-zinc-100 dark:bg-Dtab ring-1 ring-zinc-100 dark:ring-zinc-300/20">
-			{Object.keys(buttons).map((key) => (
-				<Link href={buttons[key]} key={key}>
-					<div className="relative ">
-						{key.toLocaleLowerCase() === category && (
-							<div className="absolute -z-50 flex w-12 h-2 top-0 left-0 bottom-0 right-0 mx-auto my-auto"></div>
-						)}
-						<li key={key}>
-							<Button
-								name={key}
-								style={
-									key.toLocaleLowerCase() === category
-										? { color: "#37D4BF" }
-										: undefined
-								}
-							/>
-						</li>
-					</div>
-				</Link>
-			))}
-			{/* {user?.isAdmin && (
+  const pathname = usePathname();
+  const location = pathname.split("/")[1];
+  const category = location === "" ? "home" : location;
+  return (
+    <ul className="relative z-50 flex px-2 text-h1 dark:text-white drop-shadow-md rounded-full bg-zinc-100 dark:bg-Dtab ring-1 ring-zinc-100 dark:ring-zinc-300/20">
+      {Object.keys(buttons).map((key) => (
+        <Link href={buttons[key]} key={key}>
+          <div className="relative ">
+            {key.toLocaleLowerCase() === category && (
+              <div className="absolute -z-50 flex w-12 h-2 top-0 left-0 bottom-0 right-0 mx-auto my-auto"></div>
+            )}
+            <li key={key}>
+              <Button
+                name={key}
+                style={
+                  key.toLocaleLowerCase() === category
+                    ? { color: "#37D4BF" }
+                    : undefined
+                }
+              />
+            </li>
+          </div>
+        </Link>
+      ))}
+      {/* {user?.isAdmin && (
 				<Link to="/edit/post" key="edit">
 					<li key="edit">
 						<Button
@@ -39,6 +39,6 @@ export default function Nav({ buttons }: INav) {
 					</li>
 				</Link>
 			)} */}
-		</ul>
-	);
+    </ul>
+  );
 }
